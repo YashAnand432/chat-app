f38c42416bc14617e9eecf16e4deed237bbba2f0
Yash Anand 432 <YashAnand432@users.noreply.github.com>
2024-10-29 23:44:54 +0530

    bug fixing

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -5,12 +5,11 @@ import https from "https";
 
 const app = express();
 
-const server = https.createServer(app);
+const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
 		origin: ["http://localhost:3000", "https://chat-app-project-j8jg.onrender.com"],
 		methods: ["GET", "POST"],
-		credentials : true,
 	},
 });
 
